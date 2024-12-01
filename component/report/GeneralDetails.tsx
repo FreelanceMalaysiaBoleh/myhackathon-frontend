20efeaefb4099534748aaaf5007f233b1aa991bc
unknown <annmak.shiroann@gmail.com>
2024-12-01 18:48:28 +0800

    report view page

--- component/report/GeneralDetails.tsx
+++ component/report/GeneralDetails.tsx
@@ -1,17 +1,50 @@
 const GeneralDetails = () => {
     return (
-        <div style={{ width: "100%", paddingTop: "30px" }}>
-            <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                <h2 style={{ fontSize: "24px", marginRight: "50px" }}>Case ID: </h2>
-                <h2 style={{ fontSize: "24px" }}>2024-005</h2>
+        <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
+            <div style={{ width: "40%", paddingTop: "30px" }}>
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "24px" }}>
+                    <div style={{ width: "230px" }}>
+                        <h2 style={{ fontSize: "24px" }}>Case ID: </h2>
+                    </div>
+                    <h2 style={{ fontSize: "24px" }}>2024-005</h2>
+                </div>
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "24px" }}>
+                    <div style={{ width: "230px" }}>
+                        <h2 style={{ fontSize: "24px" }}>Full Name: </h2>
+                    </div>
+                    <h2 style={{ fontSize: "24px", fontWeight: "normal" }}>Brayan Wilson</h2>
+                </div>
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "24px" }}>
+                    <div style={{ width: "230px" }}>
+                        <h2 style={{ fontSize: "24px" }}>Contact Number: </h2>
+                    </div>
+                    <h2 style={{ fontSize: "24px", fontWeight: "normal" }}>012-345 6789</h2>
+                </div>
+
             </div>
-            <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                <h2 style={{ fontSize: "24px", marginRight: "50px" }}>Full Name: </h2>
-                <h2 style={{ fontSize: "24px" }}>2024-005</h2>
+            <div style={{ width: "40%", paddingTop: "30px" }}>
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "24px" }}>
+                    <div style={{ width: "230px" }}>
+                        <h2 style={{ fontSize: "24px" }}>Date of Report: </h2>
+                    </div>
+                    <h2 style={{ fontSize: "24px", fontWeight: "normal" }}>2024-11-28</h2>
+                </div>
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "24px" }}>
+                    <div style={{ width: "230px" }}>
+                        <h2 style={{ fontSize: "24px" }}>Area: </h2>
+                    </div>
+                    <h2 style={{ fontSize: "24px", fontWeight: "normal" }}>Penang</h2>
+                </div>
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "24px" }}>
+                    <div style={{ width: "230px" }}>
+                        <h2 style={{ fontSize: "24px" }}>Email Address: </h2>
+                    </div>
+                    <h2 style={{ fontSize: "24px", color: "#073DFF", textDecoration: "underline"  }}>brayan@gmail.com</h2>
+                </div>
             </div>
-            <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                <h2 style={{ fontSize: "24px", marginRight: "50px" }}>Contact Number: </h2>
-                <h2 style={{ fontSize: "24px" }}>2024-005</h2>
+
+            <div style={{ backgroundColor: "#D70000", height: "32px", paddingLeft: 20, paddingRight: 20, borderRadius: 10, marginTop: 25 }}>
+                <h2 style={{ fontSize: "24px", color: "white" }}>High Risk</h2>
             </div>
         </div>
     )
--- pages/report/[id]/view.tsx
+++ pages/report/[id]/view.tsx
@@ -1,6 +1,10 @@
+import Divider from "@/component/main/Divider";
 import MainLayout from "@/component/main/MainLayout";
+import DetailedReport from "@/component/report/DetailedReport";
 import GeneralDetails from "@/component/report/GeneralDetails";
 import ResponseSection from "@/component/report/ResponseSection";
+import exampleImg from "../../../public/images/example_image.png"
+import Image from "next/image";
 
 const ReportView = () => {
     return (
@@ -15,10 +19,32 @@ const ReportView = () => {
                     paddingTop: 15,
                     paddingLeft: 30,
                     paddingRight: 30,
-                    paddingBottom: 15,
+                    paddingBottom: 20,
                     boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)"
                 }}>
-                    <GeneralDetails />
+                <GeneralDetails />
+                <Divider />
+                <DetailedReport />
+                <div style={{ marginTop: "30px", marginBottom: "30px" }}>
+                    <h2 style={{ fontSize: "24px" }}>Scammer&apos;s Details/Description: </h2>
+
+                    <ul style={{ listStyleType: "disc", paddingLeft: "20px", lineHeight: 1.5, marginTop: 20 }}>
+                        <li><p style={{ fontSize: "24px" }}>Name Used: John Doe</p></li>
+                        <li><p style={{ fontSize: "24px" }}>Phone Number: +65 9876 5432</p></li>
+                        <li><p style={{ fontSize: "24px" }}>Email: shopxx.support@fakeemail.com</p></li>
+                        <li><p style={{ fontSize: "24px" }}>Profile: &quot;ShopDiscount_Official&quot; on WhatsApp & Instagram</p></li>
+                    </ul>
+
+                    <p style={{ fontSize: "24px", marginTop: 40 }}>
+                        The scammer posed as Shopxx&apos;s support team, offering fake prizes and discounts. They sent messages with Shopxx’s logo, directing victims to a fake site (shopxx-prizes.com) to steal personal and bank details. Unauthorized transactions were made from victims’ accounts.
+                        Note: Verify with Shopxx&apos;s official support before sharing sensitive information.
+                    </p>
+                </div>
+
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "end" }}>
+                    <Image src={exampleImg.src} width={165} height={355} alt="image" />
+                    <p style={{marginLeft: 20}}>exampleonly.png</p>
+                </div>
             </div>
         </MainLayout>
     );
